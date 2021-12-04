23bcbee068e9f3759cc00aa5bc37f8439c046e25
martindevaluado <pmartinacosta@gmail.com>
2021-12-03 22:47:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { images, bearPasswordImage } from "./images.js";
 
-function App() {
+import "./styles.css";
+
+export default function App() {
+  const email = useRef(null);
+  const [image, setImage] = useState(images[0]);
+
+  const handleEmail = () => {
+    setImage(
+      email.current.value.length >= 20
+        ? images[images.length - 1]
+        : images[email.current.value.length]
+    );
+  };
+
+  const handlePasswordFocus = () => {
+    setTimeout(() => {
+      setImage(bearPasswordImage[0]);
+    }, 150);
+    setTimeout(() => {
+      setImage(bearPasswordImage[1]);
+    }, 300);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login</h1>
+      <img className="logo" alt="logo" src={image} />
+      <input
+        type="text"
+        placeholder="E-mail"
+        onChange={handleEmail}
+        onFocus={handleEmail}
+        ref={email}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onFocus={handlePasswordFocus}
+      />
     </div>
   );
 }
-
-export default App;
